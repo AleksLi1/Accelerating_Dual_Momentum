16f50e8da5ceb2c5019214e5cd64e9a3d48cbdfa
Aleksandras Liauska <aleksandras.v.liauska@bath.edu>
2022-09-25 15:15:01 +0300

    Compatability Update

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from functions import start_of_year
 tickers = ['VBR', 'TLT', 'TIP', 'QQQ']
 benchmark = ['SPY']
 start = '2005-01-01'
-end = '2021-07-31'
+end = '2022-01-01'
 portfolio_value = 5000  # Amount in dollars for initial portfolio value
 
 # Get and process data
@@ -21,8 +21,7 @@ monthly_ret = daily_ret.groupby(pd.Grouper(freq='M')).apply(np.sum)
 
 # Benchmark data
 prices_benchmark = yf.download(tickers=benchmark, start=start, end=end)['Adj Close']
-prices_benchmark_daily_ret = np.log(prices_benchmark / prices_benchmark.shift(1))[1:]
-prices_benchmark_daily_ret = prices_benchmark_daily_ret.to_frame()
+prices_benchmark_daily_ret = np.log(prices_benchmark / prices_benchmark.shift(1))[1:].to_frame()
 
 # Define trading dates
 month_end = pd.date_range(start, end, freq='M').strftime('%Y-%m-%d').tolist()
@@ -47,7 +46,7 @@ for col in daily_ret:
     momentum[col + '_three'] = daily_ret[col].rolling(window=63).sum()
     momentum[col + '_six'] = daily_ret[col].rolling(window=126).sum()
 momentum.dropna(inplace=True)
-prices = prices.iloc[126:]  # Remember that we are dropping na values: first 6 months
+prices = prices.iloc[126:]
 daily_ret = daily_ret[126:]
 prices_benchmark_daily_ret = prices_benchmark_daily_ret[126:]
 momentum['QQQ'] = momentum.iloc[:, :3].mean(axis=1)
@@ -63,7 +62,7 @@ momentum['signal3'] = np.select(condition_3, ['bond'])
 condition_4 = [(momentum['TLT_one'] > momentum['TIP_one'])]
 momentum['signal4'] = np.select(condition_4, ['TLT'])
 condition_5 = [(momentum['TIP_one'] > momentum['TLT_one'])]
-momentum['signal5'] = np.select(condition_5, ['TIP'])
+momentum['signal5'] = np.select(condition_5, ['TLT'])
 cols = ['signal1', 'signal2', 'signal3', 'signal4', 'signal5']
 momentum[cols] = momentum[cols].replace({str(0): ''})
 momentum['bond'] = (momentum['signal4'] + momentum['signal5'])
@@ -79,8 +78,7 @@ for idx, val in enumerate(prices_index):
             prices_index_dataset.append(momentum.iloc[[idx], [-1]])
 result = np.reshape(prices_index_dataset, (np.shape(prices_index_dataset)[0], np.shape(prices_index_dataset)[1]))
 result_df = pd.DataFrame(result, columns=['signal'])
-trading_month_start = trading_month_start[6:]
-result_df.index = trading_month_start
+result_df.index = trading_month_start[-len(result_df):]
 
 # Expand monthly signal to daily
 nyse_trading_date_range = nyse.schedule(trading_month_start[0], trading_month_end[-1])
@@ -89,14 +87,14 @@ nyse_trading_date_range_index = mcal.date_range(nyse_trading_date_range, frequen
     .tolist()
 result_df = result_df.reindex(nyse_trading_date_range_index, method='ffill').reset_index(drop=True)
 daily_ret = daily_ret.reset_index(drop=True)
-final_df = pd.concat([daily_ret, result_df], axis=1)
+final_df = pd.concat([daily_ret, result_df], axis=1)[-len(nyse_trading_date_range_index):]
 final_df.index = nyse_trading_date_range_index
 
 # Convert signal to daily ret
 final_df['signal'] = np.where(final_df['signal'] == 'VBR', final_df['VBR'], final_df['signal'])
 final_df['signal'] = np.where(final_df['signal'] == 'QQQ', final_df['QQQ'], final_df['signal'])
 final_df['signal'] = np.where(final_df['signal'] == 'TLT', final_df['TLT'], final_df['signal'])
-final_df['signal'] = np.where(final_df['signal'] == 'TIP', final_df['TIP'], final_df['signal'])
+final_df['signal'] = np.where(final_df['signal'] == 'TIP', final_df['TLT'], final_df['signal'])
 final_df['signal'] += 1
 
 # Create portfolio column
@@ -104,12 +102,14 @@ final_df['Portfolio Value'] = 0
 final_df.at[nyse_trading_date_range_index[0], 'Portfolio Value'] = portfolio_value
 final_df = final_df.reset_index(drop=False)
 final_df['Portfolio Value'] = final_df['Portfolio Value'].replace({np.nan: 0})
+final_df.loc[final_df['signal'].isna(), ['signal']] = 1
 for i, row in final_df.iterrows():
     if i == 0:
         final_df.loc[i, 'Portfolio Value'] = final_df['Portfolio Value'].iat[0]
     else:
         final_df.loc[i, 'Portfolio Value'] = final_df.loc[i, 'signal'] * final_df.loc[i - 1, 'Portfolio Value']
 final_df.drop(final_df.index[-1], inplace=True)
+final_df.to_csv('final.csv')
 
 # Create benchmark column
 prices_benchmark_daily_ret.columns = ['SPY']
